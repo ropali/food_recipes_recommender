2f8ae0501beca1a1d46873aec8981b21959b1f7f
ropali <ropali68@gmail.com>
2021-10-15 14:28:52 +0530

    data pre processing done

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-import click
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 from ..database.models import RecipeModel
 import pandas as pd
 from ..utils.logger import Logger
@@ -51,9 +49,5 @@ if __name__ == '__main__':
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
     main()
